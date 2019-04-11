a21eece42250f67ab1ca2cc0e5bab8380c4dd216
shramanpadhalni <49268016+shramanpadhalni@users.noreply.github.com>
2019-04-11 18:41:05 +0530

    Update SomeTest.java

--- src/test/java/com/geekcap/vmturbo/SomeTest.java
+++ src/test/java/com/geekcap/vmturbo/SomeTest.java
@@ -1,1 +1,17 @@
+package com.geekcap.vmturbo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by shaines on 7/24/16.
+ */
+public class SomeTest {
+
+    @Test
+    public void testN() {
+        Thing t = new Thing();
+        t.setN(5);
+        Assert.assertEquals("N should be 5", 6, t.getN());
+    }
+}
